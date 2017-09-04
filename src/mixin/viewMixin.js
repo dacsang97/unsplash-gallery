f89f819a1e5d857e8b614a6f7bc16afd64280330
SangND <dacsang97@gmail.com>
2017-09-04 12:57:50 +0700

    Show current page in address

--- src/mixin/viewMixin.js
+++ src/mixin/viewMixin.js
@@ -6,12 +6,33 @@ const mixin = {
       errors: null
     }
   },
+  created () {
+    this.currentPage = this.getCurrentPage()
+  },
   computed: {
     hasErrors () {
       return this.errors && this.errors.status && this.errors.status !== 200
     }
   },
   methods: {
+    getCurrentPage () {
+      const { query } = this.$route
+      if (!query.page) {
+        return 1
+      }
+      if (isNaN(+query.page)) {
+        return 1
+      }
+      return +query.page
+    },
+    updateCurrentPage () {
+      this.$router.push({
+        query: {
+          page: this.currentPage
+        }
+      })
+      this.currentPage ++
+    },
     handleErr (err) {
       const errors = {}
       errors.status = err.status
