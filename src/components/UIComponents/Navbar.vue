aac1c739a9fd4754d6dd9eec9f2b0fc98215a188
SangND <dacsang97@gmail.com>
2017-09-04 16:39:50 +0700

    Update navbar link

--- src/components/UIComponents/Navbar.vue
+++ src/components/UIComponents/Navbar.vue
@@ -11,7 +11,7 @@
     <b-collapse is-nav id="nav_collapse">
 
       <b-nav is-nav-bar>
-        <b-nav-item to="/">Photos</b-nav-item>
+        <b-nav-item href="/">Photos</b-nav-item>
       </b-nav>
       <b-nav is-nav-bar class="ml-auto">
         <b-nav-item href="https://github.com/dacsang97/unsplash-gallery">
